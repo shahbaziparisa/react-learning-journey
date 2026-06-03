343bdb6f44a4621f9d80f111e06d409d43b5cea3
Parisa Shahbazi <shahbazi.parisa10@gmail.com>
2026-06-03 20:00:44 +0330

    Feat : Use Link in react-router without refreshing the page

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import styles from "./App.module.css";
 import AlbumGallery from "./components/AlbumGallery";
 import Categories from "./components/Categories";
 import UserGallery from "./components/UserGallery";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { Link, Route, Routes } from "react-router";
 import LocationList from "./components/Locations";
 
 // Async / Await Example
@@ -180,14 +180,18 @@ export default function App() {
       <br />
       <Categories />
       <UserGallery />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/albums" element={<AlbumGallery />} />
-          <Route path="/categories" element={<Categories />} />
-          <Route path="/users" element={<UserGallery />} />
-          <Route path="/locations" element={<LocationList />} />
-        </Routes>
-      </BrowserRouter>
+      <Link
+        to="/locations"
+        className="px-5 py-2.5 rounded-xl bg-blue-600 text-white font-medium shadow-md hover:bg-blue-700 hover:shadow-lg active:scale-95 transition-all duration-200"
+      >
+        Route to Locations
+      </Link>
+      <Routes>
+        <Route path="/albums" element={<AlbumGallery />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/users" element={<UserGallery />} />
+        <Route path="/locations" element={<LocationList />} />
+      </Routes>
       ,
     </>
   );
